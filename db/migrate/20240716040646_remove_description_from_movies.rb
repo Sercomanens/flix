730fbc2d0c5559c621bbb8ab773e89ee3f8adae9
Laurent Sercomanens <aurovici@gmail.com>
2024-07-20 10:38:11 +0530

    Add migrations and modify the view

--- db/migrate/20240716040646_remove_description_from_movies.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDescriptionFromMovies < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :movies, :description, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_040646) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_040217) do
   create_table "movies", force: :cascade do |t|
     t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "rating"
